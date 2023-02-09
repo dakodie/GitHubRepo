fe5da90aa5ab24bc50e6af3cb12af180703ae321
dastraw <dastraw@BUS070_04>
2023-02-09 12:10:40 -0600

    Add project files.

---
+++ BasicAppTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BasicAppTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BasicAppTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("238f4eaf-c3b7-4091-8c27-ed8a1fa83c61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DS Basic App GitHub/Basic Webform.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Basic Webform.aspx.cs" Inherits=" 
+DS_Basic_App_GitHub.Basic_Webform" %> 
+ 
+<!DOCTYPE html> 
+ 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head runat="server"> 
+    <title></title> 
+</head> 
+<body> 
+    <form id="form1" runat="server"> 
+        <div> 
+            <% DS_Basic_App_GitHub.Class1 tp = new DS_Basic_App_GitHub.Class1(); %> 
+              
+            <%=tp.Name %> 
+        </div> 
+    </form> 
+</body> 
+</html> 
+ 
